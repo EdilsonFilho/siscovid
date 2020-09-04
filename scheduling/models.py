5d3c0082cf80364b59255d3f853485cde47060fd
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-09-04 00:36:18 -0300

    mexendo no BD

--- scheduling/models.py
+++ scheduling/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class Scheduling(models.Model):
     name = models.CharField(max_length=50)
+    categoryUser = models.CharField(max_length=50, null=True)
+    password = models.CharField(max_length=50, null=True)
     cpf = models.PositiveIntegerField()
-    date = models.CharField(max_length=10)
+    date = models.CharField(max_length=10, null=True)
     description = models.CharField(blank=True, null=True, max_length=500)
+    
     
 
     def __str__(self):
